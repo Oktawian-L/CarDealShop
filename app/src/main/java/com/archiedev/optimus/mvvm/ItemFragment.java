471fa62e280305f8f10ab5cd4efde15a62b605b9
Unknown <oktawianlasek@gmail.com>
2019-01-16 20:54:04 +0100

    nawiasy

--- app/src/main/java/com/archiedev/optimus/mvvm/ItemFragment.java
+++ app/src/main/java/com/archiedev/optimus/mvvm/ItemFragment.java
@@ -1,6 +1,9 @@
 package com.archiedev.optimus.mvvm;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,10 +11,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ItemFragment extends Fragment {
 
     private ItemViewModel mViewModel;
+    private ViewDataBinding binding;
 
     public static ItemFragment newInstance() {
         return new ItemFragment();
@@ -20,7 +27,9 @@ public class ItemFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.item_fragment, container, false);
+        //return inflater.inflate(R.layout.item_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.item_fragment,container,false);
+        return binding.getRoot();
     }
 
     @Override
@@ -28,6 +37,25 @@ public class ItemFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
         // TODO: Use the ViewModel
+        final Observer<Item> ItemObserver = new Observer<Item>() {
+            @Override
+            public void onChanged(@Nullable final Item newItem) {
+                ((TextView)getActivity().findViewById(R.id.iname)).setText(newItem.iname);
+                ((RatingBar)getActivity().findViewById(R.id.ratingBar)).setRating(newItem.istrength);
+                ((TextView)getActivity().findViewById(R.id.idesc)).setText(newItem.idescription);
+
+
+            }
+        };
+        mViewModel.getLDItem().observe(this,ItemObserver);
+
+        ((Button)getActivity().findViewById(R.id.button)).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mViewModel.setNewItem();
+            }
+        });
+
     }
 
 }
